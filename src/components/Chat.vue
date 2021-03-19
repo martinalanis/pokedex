77c1cb3bb505c18994d9cf4865ea7eb7a1c675cc
martinalanis <martinalanis.dev@gmail.com>
2021-03-18 20:01:40 -0600

    Chat messages styled

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="h-100 chat">
+    <div class="chat__box">
+      <div class="w-100 chat__box_container" id="chat-box">
+        <chat-message
+          v-for="({ isUser, text }, i) in conversation"
+          :key="i"
+          :is-user="isUser"
+          :text="text"
+        />
+      </div>
+    </div>
     <form
       class="w-100 relative chat__form"
       @submit.prevent="pushToConversation"
@@ -8,7 +18,7 @@
         type="text"
         class="chat__input"
         v-model="currentMessage"
-      >
+      />
       <button type="submit" class="chat__button">
         <svg class="svg-icon" viewBox="0 0 20 20">
           <path d="M17.218,2.268L2.477,8.388C2.13,8.535,2.164,9.05,2.542,9.134L9.33,10.67l1.535,6.787c0.083,0.377,0.602,0.415,0.745,0.065l6.123-14.74C17.866,2.46,17.539,2.134,17.218,2.268 M3.92,8.641l11.772-4.89L9.535,9.909L3.92,8.641z M11.358,16.078l-1.268-5.613l6.157-6.157L11.358,16.078z"></path>
@@ -19,8 +29,13 @@
 </template>
 
 <script>
+import ChatMessage from '@/components/ChatMessage'
+
 export default {
   name: 'Chat',
+  components: {
+    ChatMessage
+  },
   data () {
     return {
       conversation: [],
@@ -29,12 +44,21 @@ export default {
   },
   methods: {
     pushToConversation () {
-      // console.log('new message')
-      this.conversation.push({
-        user: 'guest',
-        message: this.currentMessage
-      })
-      this.currentMessage = ''
+      if (this.currentMessage) {
+        this.conversation.push({
+          isUser: true,
+          text: this.currentMessage
+        })
+        this.currentMessage = ''
+        this.updateScroll()
+      }
+    },
+    updateScroll () {
+      console.log('scroll update')
+      setTimeout(() => {
+        const el = document.getElementById('chat-box')
+        el.scrollTop = el.scrollHeight
+      }, 100)
     }
   }
 }
@@ -42,10 +66,23 @@ export default {
 
 <style lang="scss" scoped>
 .chat {
-  display: flex;
-  align-items: flex-end;
+  display: grid;
+  grid-template-rows: 1fr 2.5rem;
   padding: 0.8rem;
   background: #F3F6FB;
+  &__box {
+    display: flex;
+    position: relative;
+    flex-flow: column nowrap;
+    justify-content: flex-end;
+    align-items: center;
+    justify-items: flex-end;
+    overflow-y: scroll;
+    // border: 1px solid blue;
+  }
+  &__box_container {
+    overflow: scroll;
+  }
   &__form {
     display: flex;
     justify-content: center;
@@ -53,8 +90,14 @@ export default {
   &__input {
     width: 100%;
     display: block;
-    padding: 0.5rem;
-    border: 1px solid rgba(0,0,0,0.5);
+    padding: 0.3rem 1rem;
+    // border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 20px;
+    border: none;
+    box-shadow: $box-shadow;
+    &:active, &:focus, &:hover {
+      outline: none;
+    }
   }
   &__button {
     width: 3rem;
@@ -66,7 +109,7 @@ export default {
     margin-left: 0.5rem;
     border: none;
     border-radius: 50%;
-    box-shadow: $box-shadow;
+    box-shadow: $box-shadow-depth-2;
     svg {
       width: 100%;
       fill: #FFF;
---
+++ src/components/ChatMessage.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="w-100 msg" :class="{ 'bot': !isUser }">
+    <div class="msg__thumbnail">
+      <img :src="isUser ? require('@/assets/user.png') : require('@/assets/robot.png')" alt="">
+    </div>
+    <div class="msg__box">
+      <p class="msg__user">
+        {{ isUser ? 'Guest' : 'Hermes' }}
+      </p>
+      <div class="msg__text">
+        {{ text }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatMessage',
+  props: {
+    isUser: {
+      type: Boolean,
+      default: false
+    },
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$br: 15px;
+.msg {
+  // border: 1px solid teal;
+  display: flex;
+  align-items: flex-end;
+  margin-bottom: 1rem;
+  &__thumbnail {
+    width: 2.5rem;
+    height: 2.5rem;
+    border-radius: 50%;
+    // box-shadow: $box-shadow;
+    // overflow: hidden;
+    // padding: 0.4rem;
+    // background: #5199E4;
+    // &--user {
+    // }
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      object-fit: contain;
+    }
+  }
+  &__box {
+    // border: 1px solid #fabada;
+    margin: 0 1rem;
+    max-width: 75%;
+  }
+  &__user {
+    font-size: 0.8rem;
+    color: #656468;
+    font-weight: 700;
+    margin-bottom: 0.6rem;
+    // padding: 0 0.9rem;
+  }
+  &__text {
+    display: inline-block;
+    line-height: 1.3;
+    background: #FFF;
+    padding: 0.6rem 0.9rem;
+    border-radius: $br 0px $br 0px;
+    box-shadow: $box-shadow;
+  }
+}
+.bot {
+  flex-direction: row-reverse;
+  .msg__box {
+    text-align: right;
+  }
+  .msg__text {
+    border-radius: 0px $br 0px $br;
+  }
+}
+</style>
